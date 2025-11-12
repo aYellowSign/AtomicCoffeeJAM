375323007d0b9b8b6c6b751bcfb2158f3e62e7ca
aYellowSign <xavierdhennezel1@gmail.com>
2025-11-12 11:30:12 +0100

    DialogueScriptableObject

---
+++ Assets/Scripts/DialogueData.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialogue", menuName = "Dialogue/Dialogue Data")]
+public class DialogueData : ScriptableObject
+{
+    [Serializable]
+    public class DialogueLine
+    {
+        public string characterName;
+        [TextArea(2,5)] public string dialogueText;
+    }
+
+    public List<DialogueLine> lines = new List<DialogueLine>();
+}
